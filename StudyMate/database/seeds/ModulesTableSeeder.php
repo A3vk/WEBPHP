e87fc2318d9422cef65cff14da86aa5dd9817b3c
Kevin Treffers <kevintreffers@msn.com>
2020-03-26 15:45:31 +0100

    seed fix

--- StudyMate/database/seeds/ModulesTableSeeder.php
+++ StudyMate/database/seeds/ModulesTableSeeder.php
@@ -22,7 +22,7 @@ class ModulesTableSeeder extends Seeder
             ['name' => 'Rapporteren', 'period' => 4, 'block' => 4, 'total_credits' => 1, 'obtained_credits' => 1,'created_at' => Now(), 'updated_at' => Now() ],
             ['name' => 'Programmen 5', 'period' => 1, 'block' => 5, 'total_credits' => 3, 'obtained_credits' => 3,'created_at' => Now(), 'updated_at' => Now() ],
             ['name' => 'Algoritmiek', 'period' => 1, 'block' => 5, 'total_credits' => 3, 'obtained_credits' => 3,'created_at' => Now(), 'updated_at' => Now() ],
-            ['name' => 'Algoritmiek', 'period' => 2, 'block' => 6, 'total_credits' => 3, 'obtained_credits' => 3,'created_at' => Now(), 'updated_at' => Now() ],
+            ['name' => 'Programmeren 6', 'period' => 2, 'block' => 6, 'total_credits' => 3, 'obtained_credits' => 3,'created_at' => Now(), 'updated_at' => Now() ],
             ['name' => 'Gesprekken', 'period' => 2, 'block' => 6, 'total_credits' => 1, 'obtained_credits' => 1,'created_at' => Now(), 'updated_at' => Now() ],
             ['name' => 'WebJs', 'period' => 3, 'block' => 7, 'total_credits' => 2, 'obtained_credits' => 0,'created_at' => Now(), 'updated_at' => Now() ],
             ['name' => 'WebPHP', 'period' => 3, 'block' => 7, 'total_credits' => 2, 'obtained_credits' => 0,'created_at' => Now(), 'updated_at' => Now() ],
