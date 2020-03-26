1b0d78ee49ffa3753ffc76b6c8d2d1c6ece0c5c0
Kevin Treffers <kevintreffers@msn.com>
2020-03-26 18:57:53 +0100

    trait fix

--- StudyMate/app/Traits/Encryptable.php
+++ StudyMate/app/Traits/Encryptable.php
@@ -1,15 +1,24 @@
-namespace App\Traits
+<?php
 
-trait Encryptable{
+namespace App\Traits;
 
-    public function($key) 
+trait Encryptable
+{
+    public function getAttribute($key)
     {
         $value = parent::getAttribute($key);
 
-        if ($value != null){
-            if (in_array($key, $this->encryptable)){
-                $value = Crypt::decrypt($value);
-            }
+        if (in_array($key, $this->encryptable)) {
+            $value = Crypt::decrypt($value);
         }
+    }
+
+    public function setAttribute($key, $value)
+    {
+        if (in_array($key, $this->encryptable)) {
+            $value = Crypt::encrypt($value);
+        }
+
+        return parent::setAttribute($key, $value);
     }
 }
