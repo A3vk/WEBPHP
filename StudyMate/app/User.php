12c2b5d3c62d90588ffebcdd62d09fafbaab9cad
Kevin Treffers <kevintreffers@msn.com>
2020-03-27 13:08:43 +0100

    user code layout fix

--- StudyMate/app/User.php
+++ StudyMate/app/User.php
@@ -19,7 +19,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'password',
+        'username', 
+        'password',
     ];
 
     protected $encryptable = [
@@ -32,7 +33,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
