c71ef57a57f9c22006f7cdfbebfefb8289700c84
Kevin Treffers <kevintreffers@msn.com>
2020-03-28 19:15:24 +0100

    Admin dusk tests
    added 3 dusk tests for the admin
    1 to check if admind login shows button
    2 go to admin screen admin/teachers when loged in
    3 add new teacher

---
+++ StudyMate/tests/Browser/AdminTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\User;
+
+class AdminTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+
+    /** @test */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $this->browse(function ($browser)
+             {
+                $browser->visit('/login')
+                        ->type('username', 'Admin')
+                        ->type('password', 'Admin123')
+                        ->press('Login')
+                        ->visit('/')
+                        ->assertSee('Admin');
+            });
+        });
+    }
+
+    /** @test */
+    public function adminCanAccessAdminEnvoirment()
+    {
+        $this->browse(function (Browser $browser) {
+            $this->browse(function ($browser)
+             {
+                $user = new User([
+                    'username' => 'Admin',
+                    'password' => 'carl',
+                    'role' => 'admin'
+                ]);
+
+                $browser->loginAs($user)
+                        ->visit('/')
+                        ->clickLink('Admin')
+                        ->AssertPathIs('/admin/teachers');
+            });
+        });
+    }
+
+    /** @test */
+    public function adminCanCreateTeacher()
+    {
+        $this->browse(function (Browser $browser) {
+            $this->browse(function ($browser)
+             {
+                $user = new User([
+                    'username' => 'Admin',
+                    'password' => 'carl',
+                    'role' => 'admin'
+                ]);
+
+                $browser->loginAs($user)
+                        ->visit('/admin/teachers/create')
+                        ->type('first_name', 'Carl')
+                        ->type('last_name', 'Carlson')
+                        ->press('Opslaan')
+                        ->assertSee('Docent opgeslagen!');
+            });
+        });
+    }
+}
